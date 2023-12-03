cc20326cbbec5c638b2d4ad23fb5f3d40f476897
SadShrimpyy <B0ll3D!bH1@cCI()>
2023-12-03 22:03:16 +0100

    Ready, set, go!

---
+++ src/main/java/com/shiry/Main.java
@@ -0,0 +1,9 @@
+package com.shiry;
+
+import com.shiry.days.Day1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("«Day 1» Number: " + new Day1().calc(100));
+    }
+}
